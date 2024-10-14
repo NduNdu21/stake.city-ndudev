7cc259da4238deeee8955018eb37a4570f6cac5a
NduNdu21 <ndumisobm@icloud.com>
2024-10-14 16:41:57 +0100

    Adding basic structure to the page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Layout() {
     return (
         <section className="py-24">
-            
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="grid lg:grid-cols-2 grid-cols-1">
+                    <div className="lg:mb-0 mb-10">
+                        
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
