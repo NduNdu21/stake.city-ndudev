e939f80f3427edcd4873a3cec22ec932cfac5df8
NduNdu21 <ndumisobm@icloud.com>
2024-10-14 17:41:22 +0100

    Added form section and heading

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,8 +10,15 @@ export default function Layout() {
                         <div className="group w-full h-full">
                             <div className="relative h-full">
                                 <img src={bgImage} alt="" className="w-full h-full lg:rounded-l-2xl rounded-2xl bg-blend-multiply bg-[#0D1B2A] object-cover"/>
+                                <h1 className="text-[#F0F3F5] text-4xl font-bold leading-10 absolute top-11 left-11">Report Bugs</h1>
+                                <div className="absolute bottom-0 w-full lg:p-11 p-5">
+                                    {/* Section for logos and contact details if needed */}
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <div className="bg-[#0D1B2A] p-5 lg:p-11 lg:rounded-r-2xl rounded-2xl">
+                        <h2 className="text-4xl font-semibold leading-10 mb-11">Send us a message</h2>
                     </div>
                 </div>
             </div>
