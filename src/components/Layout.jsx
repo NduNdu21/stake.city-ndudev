03a901ac90c570b5283b26163c289afb2c36bae7
NduNdu21 <ndumisobm@icloud.com>
2024-10-17 15:53:32 +0100

    changed headings again

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -24,15 +24,16 @@ export default function Layout() {
                                 <img src={moon} alt="moon" className={`absolute left-8 sm:left-16 md:left-32 bottom-2 sm:bottom-5 w-28 sm:w-48 md:w-64 h-auto ${styles.float}`}/>
                                 <img src={rocket} alt="rocket" className={`absolute right-2 sm:right-4 md:right-26 top-2 sm:top-5 w-28 sm:w-48 md:w-64 h-auto ${styles.float}`}/>
 
-                                <h1 className="text-[#F0F3F5] text-4xl font-bold leading-10 absolute top-11 left-11 opacity-0">Report</h1>
+                                <h1 className="text-[#F0F3F5] text-4xl font-bold leading-10 absolute top-11 left-11">Found an issue?</h1>
                                 <div className="absolute bottom-0 w-full lg:p-11 p-5">
                                     {/* Section for logos and contact details if needed */}
+                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                     <form action="POST" className="relative bg-[#0D1B2A]/85 p-5 lg:p-11 lg:rounded-r-2xl rounded-2xl shadow-md shadow-[#A0AAB2]">
-                        <h2 className="text-[#F0F3F5] text-3xl font-semibold leading-10 mb-10">Found an issue?</h2>
+                        <h2 className="text-[#F0F3F5] text-3xl font-semibold leading-10 mb-10">Report Bugs</h2>
                         <input type="text" className="w-full h-8 sm:h-10 text-[#F0F3F5] placeholder-gray-400  shadow-sm bg-transparent text-lg font-normal leading-7 rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Name"/>
                         <input type="text" className="w-full h-8 sm:h-10 text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Username"/>
                         <input type="text" className="w-full h-8 sm:h-10 text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Email"/>
