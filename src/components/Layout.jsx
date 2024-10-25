188e696c94a3e288ab3ff8514e80a3506757e3e6
NduNdu21 <ndumisobm@icloud.com>
2024-10-25 09:54:17 +0100

    changed justify from stretch to evenly and removed the gap

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,6 +8,7 @@ import logo from "../logo7.svg"
 import graphic from "../imgs/report graphic.svg"
 
 
+
 export default function Layout() {
     const [selectedFile, setSelectedFile] = useState(null);
 
@@ -19,7 +20,7 @@ export default function Layout() {
         <div className="bg-wavy-pattern bg-cover bg-[#0D1B2A] bg-no-repeat min-h-screen py-12">
             {/* <h1 className="text-[#F0F3F5] text-4xl font-bold leading-10 absolute top-11 left-11">Found an issue?</h1> */}
             
-            <div className="flex flex-row justify-stretch gap-10">
+            <div className="flex flex-row justify-evenly px-10">
                 <div>
                     {/* <img src={moon} alt="moon" className={`w-52 h-auto ${styles.float}`} />
                     <img src={rocket} alt="rocket" className={`w-52 h-auto ${styles.float}`} />
@@ -28,7 +29,7 @@ export default function Layout() {
                 </div>
                 <form action="POST" className=" bg-[#0D1B2A]/85 p-5 w-64 rounded-2xl shadow-md shadow-[#A0AAB2]">
                     <h2 className="text-[#F0F3F5] text-3xl font-semibold mb-10">Report it here:</h2>
-                    <input type="text" className="h-8 text-[#F0F3F5] placeholder-gray-400  shadow-sm bg-transparent text-lg font-normal rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Name" />
+                    <input type="text" className="h-8 w-full text-[#F0F3F5] placeholder-gray-400  shadow-sm bg-transparent text-lg font-normal rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Name" />
                     <input type="text" className="h-8 text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Username" />
                     <input type="text" className="h-8 text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal rounded-full border border-[#A0AAB2] focus:outline-none pl-4 mb-7" placeholder="Email" />
                     <div className="relative inline-block mb-7">
