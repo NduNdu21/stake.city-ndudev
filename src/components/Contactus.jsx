0f8828a13f5aec17dcacf9050d50f555fcba5e8a
NduNdu21 <ndumisobm@icloud.com>
2024-11-11 15:38:17 +0000

    new graphic added and size adjusted

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -1,5 +1,5 @@
 import React from "react"; 
-import graphic from "../imgs/graphic.png"
+import graphic2 from "../imgs/graphic 2.svg"
 import discord from "../imgs/discord-icon.svg"
 import twitter from "../imgs/x-icon.svg"
 import { HiLocationMarker, HiMail, HiPhone } from "react-icons/hi";
@@ -8,10 +8,9 @@ import { HiLocationMarker, HiMail, HiPhone } from "react-icons/hi";
 export default function ContactUs() {
     return (
         <div className="bg-space-bg bg-cover bg-no-repeat min-h-screen py-12">
-
-            <div className="flex flex-row flex-wrap justify-evenly px-10">
+            <div className="flex flex-row flex-wrap md:flex-nowrap justify-evenly px-10">
                 <h1 className="text-[#F0F3F5] text-3xl font-semibold mb-7">Contact Us</h1>
-                <form action="POST" className="bg-gradient-to-r from-[#0d1b2a]/80 to-[#20c999]/80 rounded-lg border-4 border-[#f0f3f5] p-5 w-fit h-auto">
+                <form action="POST" className="bg-gradient-to-r from-[#0d1b2a]/80 to-[#20c999]/80 rounded-lg border-4 border-[#f0f3f5] p-5 w-fit md:w-3/5 h-auto">
                     <input type="text" className="h-8 w-full text-[#F0F3F5] placeholder-gray-400  shadow-sm bg-[#0D1B2A] text-lg font-normal rounded-lg border border-[#A0AAB2] focus:outline-none pl-4 mb-5" placeholder="Name" />
                     <input type="text" className="h-8 w-full text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-[#0D1B2A] text-lg font-normal rounded-lg border border-[#A0AAB2] focus:outline-none pl-4 mb-5" placeholder="Username" />
                     <input type="text" className="h-8 w-full text-[#F0F3F5] placeholder-gray-400 shadow-sm bg-[#0D1B2A] text-lg font-normal rounded-lg border border-[#A0AAB2] focus:outline-none pl-4 mb-5" placeholder="Email" />
@@ -19,7 +18,7 @@ export default function ContactUs() {
                     <button className="h-8 w-full text-white text-base font-semibold rounded-full transition-all duration-700 bg-[#20C997]">Send</button>
                 </form>
                 <div>
-                    <img src={graphic} alt="graphic" className="w-48 h-48" />
+                    <img src={graphic2} alt="graphic" className="w-80 h-80" />
                     <div id="contact" className="flex m-5"> 
                         <HiLocationMarker className="w-5 h-5 text-[#F0F3F5] mx-1" />
                         <p className="text-[#F0F3F5]">London, England</p>
